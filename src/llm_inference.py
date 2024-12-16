3b2fb71b669eb4113aa625bded97af393e01fc66
Marios-Mamalis <46795338+Marios-Mamalis@users.noreply.github.com>
2024-12-16 16:50:57 +0200

    added llm inference

---
+++ src/llm_inference.py
@@ -0,0 +1,49 @@
+from openai import AzureOpenAI
+from openai._exceptions import RateLimitError
+import typing
+import logging
+import os
+from retrying import retry
+
+
+os.makedirs('../logs', exist_ok=True)
+logging.basicConfig(filename='../logs/llm_app.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+@retry(stop_max_attempt_number=5, wait_exponential_multiplier=1000,
+       retry_on_exception=lambda exept: isinstance(exept, RateLimitError))
+def openai_inference(conversation_history: typing.List[typing.Dict[str, str]], user_query: str,
+                     api_key: str, api_version: str, endpoint: str,
+                     deployment: str) -> typing.List[typing.Dict[str, str]]:
+    """
+    Performs inference with an OpenAI model.
+    :param user_query: The string of the user query.
+    :param conversation_history: The conversational history in the form specified by the OpenAI library. Specifically:
+    conversation_history = [{"role": "user", "content": "Input"},
+                            {"role": "assistant", "content": "Output"}, ...]
+    :param api_key: model api_key configuration
+    :param api_version: model api_version configuration
+    :param endpoint: model endpoint configuration
+    :param deployment: model deployment configuration
+    :return: The conversational history with the addition of the last user-assistant appended on the end, in the same
+    format.
+    """
+
+    # Initialize client
+    try:
+        client = AzureOpenAI(api_key=api_key, api_version=api_version, azure_endpoint=endpoint)
+    except Exception as e:
+        logging.error(f'Client initialization failed: {e}')
+        raise e
+
+    # assert that the last dialogue was done by the model
+    if conversation_history:  # if not new conversation
+        assert conversation_history[-1]['role'] == 'assistant', 'Convesation must end with the assistant writing.'
+
+    new_conversation_history = conversation_history + [{"role": "user", "content": user_query}]
+    completion = client.chat.completions.create(model=deployment, messages=new_conversation_history)
+    new_conversation_history = (new_conversation_history +
+                                [{"role": "assistant", "content": completion.choices[0].message.content}])
+
+    return new_conversation_history
---
+++ tests/test_llm_inference.py
@@ -0,0 +1,47 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from src.llm_inference import openai_inference
+
+
+class TestOpenaiInference:
+    def set_nontest_vals(self):
+        self.api_key = "api_key"
+        self.api_version = "api_version"
+        self.endpoint = "endpoint"
+        self.deployment = "deployment"
+
+
+    @patch("src.llm_inference.AzureOpenAI")
+    def test_openai_inference_valid(self, MockAzureOpenAI):
+        self.set_nontest_vals()
+        self.conversation_history = [
+            {"role": "user", "content": "Input"},
+            {"role": "assistant", "content": "Output"}
+        ]
+        self.user_query = "user_query"
+
+        mock_client = MagicMock()
+        MockAzureOpenAI.return_value = mock_client
+        mock_client.chat.completions.create.return_value.choices = [MagicMock(message=MagicMock(content="Response"))]
+
+        result = openai_inference(self.conversation_history, self.user_query, self.api_key, self.api_version,
+                                  self.endpoint, self.deployment)
+
+        assert len(result) == 4  # assert correct number of dialogue truns
+        assert result[-1]['role'] == 'assistant'  # assert that the last dialogue belongst to the assistant
+        assert result[-1]['content'] == "Response"  # assert that the response was correctly inserted
+
+
+    @patch("src.llm_inference.AzureOpenAI")
+    def test_invalid_conversation_history(self, MockAzureOpenAI):
+        self.set_nontest_vals()
+        self.conversation_history = [{"role": "user", "content": "Input"}]
+        self.user_query = "user_query"
+
+        mock_client = MagicMock()
+        MockAzureOpenAI.return_value = mock_client
+        mock_client.chat.completions.create.return_value.choices = [MagicMock(message=MagicMock(content="Response"))]
+
+        with pytest.raises(AssertionError):
+            openai_inference(self.conversation_history, self.user_query, self.api_key, self.api_version,
+                             self.endpoint, self.deployment)
