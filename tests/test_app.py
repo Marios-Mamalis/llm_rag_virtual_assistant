fe2005c9697ae9c6ed64125b6cb67d242d719a20
Marios-Mamalis <46795338+Marios-Mamalis@users.noreply.github.com>
2024-12-18 16:43:21 +0200

    intermediate commit for src.app tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,13 @@
 from fastapi.testclient import TestClient
 from src.app import app
 
+
 test_client = TestClient(app)
+
 
 class TestCheckStatus:
     def test_check_status_code(self):
         assert test_client.get("/health").status_code == 200
+
     def test_check_status_message(self):
         assert test_client.get("/health").json() == {"status message": "Server is up."}
